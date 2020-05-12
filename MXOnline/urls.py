a3e48171843a7802434ba2e51c2e5a668e4f8a3d
孙洛喜 <64411917+luosifan-bbaa@users.noreply.github.com>
2020-05-12 18:28:55 +0800

    课程相关

--- MXOnline/urls.py
+++ MXOnline/urls.py
@@ -1,5 +1,4 @@
-"""MXOnline URL Configuration
-
+"""MxOnline URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -13,25 +12,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from django.views.static import serve
-
 import xadmin
-from MXOnline.settings import MEDIA_ROOT
-from apps.organizations.views import OrgView
 # from apps.users import views
 from django.views.generic import TemplateView
+from apps.organizations.views import OrgView
 from apps.users.views import LoginView
+from django.conf.urls import url, include
+from django.views.static import serve
+from MXOnline.settings import MEDIA_ROOT
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('xadmin/', xadmin.site.urls),
     # path('', views.index),
-    path('',TemplateView.as_view(template_name='index.html'), name = 'index'),
-    path('login/',LoginView.as_view(), name = 'login'),
-    #配置授课机构列表展示
-    path('orglist/',OrgView.as_view(), name = 'org_list'),
+    path('',TemplateView.as_view(template_name='index.html'), name='index'),
+    path('courselist/',TemplateView.as_view(template_name='course-list.html'), name='course-list'),
+    path('login/',LoginView.as_view(), name ='login'),
+    # 配置授课机构列表展示
+    # path('orglist/',OrgView.as_view(), name='org_list'),
+    # 授课机构相关操作
+    url(r'^org/', include(('apps.organizations.urls', 'organizations'), namespace='org')),
     # 配置上传文件的访问url
     url(r'^media/(?P<path>.*)$', serve, {"document_root": MEDIA_ROOT}),
+    # 课程相关页面
+    url(r'^course/', include(('apps.courses.urls', 'courses'), namespace='course')),
+
 ]
--- apps/organizations/views.py
+++ apps/organizations/views.py
@@ -1,6 +1,10 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import View
 from apps.organizations.models import CourseOrg, City, Teacher
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+from apps.organizations.forms import AddAskForm
+
 # Create your views here.
 class OrgView(View):
     def get(self, request, *args, **kwargs):
@@ -12,9 +16,59 @@ class OrgView(View):
         :return:
         """
         all_orgs = CourseOrg.objects.all()
-        org_nums = CourseOrg.objects.all().count()
         all_citys = City.objects.all()
+        hot_orgs = all_orgs.order_by("-click_nums")[:3]
+        # 获取点击的类目
+        category = request.GET.get("ct","")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
 
+        # 对所在城市进行筛选
+        city_id = request.GET.get('city',"")
+        if city_id:
+            if city_id.isdigit():
+                all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 对课程机构近排序
+        sort = request.GET.get('sort',"")
+        if sort == 'students':
+            # 根据学生人数排序  减号代表倒序排序的意思
+            all_orgs = all_orgs.order_by('-students')
+        elif sort == 'courses':
+            # 根据课程数进行排序
+            all_orgs = all_orgs.order_by('-course_nums')
+
+        org_nums = all_orgs.count()
+
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        p = Paginator(all_orgs,per_page=5, request=request) # 每页显示多少个
+        orgs = p.page(page)
 
         return render(request, 'org-list.html',
-                      {'all_orgs':all_orgs,'org_nums':org_nums,'all_citys':all_citys})
+                      {'city_id':city_id,
+                       'all_orgs':orgs,
+                       'org_nums':org_nums,
+                       'all_citys':all_citys,
+                       'category':category,
+                       'sort':sort,
+                       'hot_orgs':hot_orgs,
+                       })
+
+class AddAsk(View):
+    """处理用户咨询模块"""
+    def post(self, request, *args, **kwargs):
+        userask_form = AddAskForm(request.POST)
+        if userask_form.is_valid():
+            userask_form.save(commit=True)
+            return JsonResponse({
+                "status":"success",
+                "msg":"提交成功"
+            })
+        else:
+            return JsonResponse({
+                "status": "fail",
+                "msg": "添加出错"
+            })
